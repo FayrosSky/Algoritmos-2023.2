24580d1d6fd34314ff0c229ccecf6355fdf337e9
LucasLopes <davydlucassky@gmail.com>
2023-10-27 00:05:40 -0300

    Algoritmos 2023.2

---
+++ string/teste.js
@@ -0,0 +1,16 @@
+import { print } from "../utils/input.js";
+
+function main(){
+    
+const a = [1, 2, 3, 4, 5]
+
+for(const numero of a){
+  if (numero % 2 == 0){
+    print(`${numero}`)
+  } 
+}
+
+
+
+}
+main()
